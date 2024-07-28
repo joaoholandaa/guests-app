6cf37b6ec48c7de16974a01afcb4ccd8b1e38def
Joao Holanda <joaocmdt505@gmail.com>
2024-07-27 21:14:19 -0300

    feat: Add package connection with context in app and create guest model

--- app/src/main/java/com/holandadev/guests/repository/GuestRepository.kt
+++ app/src/main/java/com/holandadev/guests/repository/GuestRepository.kt
@@ -1,19 +1,27 @@
 package com.holandadev.guests.repository
 
-class GuestRepository private constructor() {
+import android.content.Context
+import androidx.core.content.ContextCompat
+
+class GuestRepository private constructor(context: Context) {
+
+    private val guestDataBase = GuestDataBase(context)
 
     // Singleton
     companion object {
         private lateinit var repository: GuestRepository
 
-        fun getInstance(): GuestRepository {
+        fun getInstance(context: Context): GuestRepository {
             if (!Companion::repository.isInitialized) {
-                repository = GuestRepository()
+                repository = GuestRepository(context)
             }
             return repository
         }
     }
 
     fun save() {
+    }
+
+    fun update() {
     }
 }
